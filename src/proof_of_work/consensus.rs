19f7a62e05ec4e382df55e07ae9b63cd6755ed32
Marvin Hsu <marv.hsu@icloud.com>
2023-05-18 15:41:16 +0000

    define struct

--- src/proof_of_work/consensus.rs
+++ src/proof_of_work/consensus.rs
@@ -1,13 +1,73 @@
-pub struct Block {}
+/// 交易內容
+pub struct Transaction {
+    pub sender: String,
+    pub receiver: String,
+    pub amount: u32,
+    pub fee: u32,
+    pub message: String,
+}
+
+/// 區塊
+pub struct Block {
+    pub previous_hash: String,
+    pub hash: Option<String>,
+    pub difficuity: u32,
+    pub timestamp: Option<u32>,
+    pub nonce: Option<u32>,
+    pub transactions: Vec<Transaction>,
+    pub miner: Option<String>,
+    pub miner_rewards: u32,
+}
+
+/// 區塊鏈
+pub struct BlockChain {
+    pub adjust_difficuity_unit: u32,
+    pub difficuity: u32,
+    pub block_time: u32,
+    pub mining_rewards: u32,
+    pub block_limitation: u32,
+    pub chain: Vec<Block>,
+    pub pending_transaction: Vec<Transaction>,
+}
+
+impl Transaction {
+    pub fn new(sender: String, receiver: String, amount: u32, fee: u32, message: String) -> Self {
+        Self {
+            sender,
+            receiver,
+            amount,
+            fee,
+            message,
+        }
+    }
+}
 
 impl Block {
-    pub fn new(
-        block_data: &str,
-        previous_block_hash: &str,
-        target_difficulty: u32,
-        nonce: usize,
-    ) -> Block {
-        Block {}
+    pub fn new(previous_hash: &str, difficuity: u32, miner_rewards: u32) -> Block {
+        Block {
+            previous_hash: previous_hash.to_owned(),
+            hash: None,
+            difficuity,
+            timestamp: None,
+            nonce: None,
+            transactions: vec![],
+            miner: None,
+            miner_rewards,
+        }
+    }
+}
+
+impl BlockChain {
+    pub fn new() -> Self {
+        Self {
+            adjust_difficuity_unit: 10,
+            difficuity: 1,
+            block_time: 30,
+            mining_rewards: 10,
+            block_limitation: 32,
+            chain: vec![],
+            pending_transaction: vec![],
+        }
     }
 }
 
